508c81e5873c87def2b285bebedbde7273d959b2
sangraze <sangrazeali22@gmail.com>
2024-11-25 10:10:09 +0400

    profile photo url changed

--- src/components/global-components/ProfilePopOver.tsx
+++ src/components/global-components/ProfilePopOver.tsx
@@ -18,7 +18,7 @@ const ProfilePopOver = ({ setDropdownOpen, setIsLogoutClicked }: Props) => {
   const user = {
     username: userData?.userName,
     email: userData?.email,
-    profilePicture: userData?.profilePhoto,
+    profilePicture: userData?.profilePhoto?.url,
   };
 
   const handleLogout = () => {
