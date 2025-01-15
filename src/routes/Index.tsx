c46508c911e13948a4a7dddd4ae9522626038607
sangraze <sangrazeali22@gmail.com>
2025-01-15 13:44:36 +0400

    new

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -8,14 +8,22 @@ const routes = [
   {
     path: constantPaths.HOME,
     element: (
-      <Suspense fallback={<div className="w-full min-h-screen flex justify-center items-center"> <Spinner color="warning" /></div>
-      } >
+      <Suspense fallback={<div className="w-full min-h-screen flex justify-center items-center"> <Spinner color="warning" /></div>}>
         <div className='px-8'>
           <Home />
         </div>
       </Suspense>
     ),
   },
+  {
+    path: '*',
+    element: <RedirectToHome />
+  }
 ];
+
+function RedirectToHome() {
+  window.location.href = constantPaths.HOME;
+  return null;
+}
 
 export default routes;
